620067e85c38f25db47c3fc4cbfb02a9bb171194
Dongwook Lee <replon87@gmail.com>
2021-01-14 15:41:12 +0900

    refact: reduce duplicated code

--- rexearch/rexearch.py
+++ rexearch/rexearch.py
@@ -18,78 +18,54 @@ class Rexearch:
         self.custom_functions = dict()
 
     def load(self, rules):
-        self.rules = rules
+        self.rules = []
         # Compile rules in advance
         if self.mode is SEARCH_MODE.SEPARATED or self.mode is SEARCH_MODE.MULTI_THREAD:
-            for i, rule in enumerate(self.rules):
+            for i, rule in enumerate(rules):
+                if 'regex' not in rule:
+                    print(f'Cannot find "regex" field. ignored : {str(rule)}')
+                    continue
                 if "id" not in rule and self.auto_rule_id:
                     rule["id"] = f"UNNAMED_RULE_{i}"
                 rule["regex_compiled"] = re.compile(rule["regex"])
-        if self.mode is SEARCH_MODE.UNIFIED:
+                self.rules.append(rule)
+        elif self.mode is SEARCH_MODE.UNIFIED:
             offset = 0
             self.group_num_to_rule_num = dict()
             unified_regex_str = ""
-            for i, rule in enumerate(self.rules):
+            for i, rule in enumerate(rules):
+                if 'regex' not in rule:
+                    print(f'Cannot find "regex" field. ignored : {str(rule)}')
+                    continue
                 if "id" not in rule and self.auto_rule_id:
                     rule["id"] = f"UNNAMED_RULE_{i}"
                 unified_regex_str = "|".join([unified_regex_str, "(" + rule["regex"] + ")"])
                 rule["group_offset"] = offset + 1
                 self.group_num_to_rule_num[offset + 1] = i
                 offset += re.compile(rule["regex"]).groups + 1
+                self.rules.append(rule)
             self.unified_regex = re.compile(unified_regex_str)
+
+        if len(self.rules) > 0:
+            print(f'{len(self.rules)} rules loaded')
+        else:
+            raise ValueError(f"Cannot find rules")
 
     def load_json_file(self, filepath, encoding="utf-8"):
         with open(filepath, mode="rt", encoding=encoding) as ifs:
-            self.load(json.load(ifs))
+            obj = json.load(ifs)
+            if "rules" in obj:
+                self.load(obj["rules"])
+            elif type(obj) is list:
+                self.load(obj)
+            else:
+                raise ValueError(f"Cannot find rules in the file {filepath}")
 
     def search(self, input_str: str, return_match_obj=False):
         if self.mode is SEARCH_MODE.SEPARATED:
             result = []
             for i, rule in enumerate(self.rules):
-                result.extend(self.__search(rule, input_str, return_match_obj))
-
-            return result
-
-        elif self.mode is SEARCH_MODE.UNIFIED:
-            result = []
-            for match in self.unified_regex.finditer(input_str):
-                for group_num, rule_num in self.group_num_to_rule_num.items():
-                    whole_raw = match.group(group_num)
-                    if whole_raw is None or whole_raw == "":
-                        continue
-
-                    rule = self.rules[rule_num]
-                    offset = rule["group_offset"]
-                    target_regex_group = rule.get("target_regex_group") or 0
-                    target_regex_group += offset
-                    raw = match.group(target_regex_group)
-                    representation = rule.get("repr")
-                    categories = rule.get("categories")
-                    rule_id = rule.get("id")
-
-                    # Parse representation if '{}' exists in it
-                    representation_out = representation
-                    if representation is not None and "{" in representation and "}" in representation:
-                        group = match.group  # noqa: F841
-                        custom_function = self.custom_functions  # noqa: F841
-                        representation = re.sub("(group\\([0-9]+)(\\))", repl=f"\\1+{offset}\\2", string=representation)
-                        representation_out = eval('f"' + representation + '"')
-
-                    start, end = match.span(target_regex_group)
-                    item = {"raw": raw, "start": start, "end": end}
-
-                    # Additional metadata
-                    if representation_out is not None:
-                        item["repr"] = representation_out
-                    if rule_id is not None:
-                        item["rule_id"] = rule_id
-                    if categories is not None:
-                        item["categories"] = categories
-
-                    if return_match_obj:
-                        print("WARN: return_match_obj is NOT supported in Unified mode (ignored)")
-
-                    result.append(item)
+                result.extend(self.__search_by_a_rule(rule, input_str, return_match_obj))
 
             return result
 
@@ -98,47 +74,71 @@ class Rexearch:
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 futures = []
                 for i, rule in enumerate(self.rules):
-                    futures.append(executor.submit(self.__search, rule, input_str, return_match_obj))
+                    futures.append(executor.submit(self.__search_by_a_rule, rule, input_str, return_match_obj))
 
                 for future in futures:
                     results.extend(future.result())
 
             return results
 
+        elif self.mode is SEARCH_MODE.UNIFIED:
+            if return_match_obj:
+                print("WARN: return_match_obj=True is NOT supported in SEARCH_MODE.UNIFIED (ignored)")
+
+            result = []
+            for match in self.unified_regex.finditer(input_str):
+                for group_num, rule_num in self.group_num_to_rule_num.items():
+                    group_raw = match.group(group_num)
+                    if group_raw is None or group_raw == "":
+                        continue
+
+                    result.append(self.__make_result_item(self.rules[rule_num], match))
+
+            return result
+
         else:
             raise ValueError(f"Unknown Mode: {self.mode}")
 
-    def __search(self, rule, input_str, return_match_obj):
+    def __search_by_a_rule(self, rule, input_str, return_match_obj):
         result = []
-        target_regex_group = rule.get("target_regex_group") or 0
-        representation = rule.get("repr")
-        categories = rule.get("categories")
-        rule_id = rule.get("id")
-
         for match in rule["regex_compiled"].finditer(input_str):
-            raw = match.group(target_regex_group)
+            result.append(self.__make_result_item(rule, match, return_match_obj))
+        return result
 
-            # Parse representation if '{}' exists in it
-            representation_out = representation
-            if representation is not None and "{" in representation and "}" in representation:
+    def __make_result_item(self, rule, match, return_match_obj=False):
+        offset = rule.get("group_offset") or 0
+        target_regex_group = rule.get("target_regex_group") or 0
+        target_regex_group += offset
+
+        raw = match.group(target_regex_group)
+        start, end = match.span(target_regex_group)
+        item = {"raw": raw, "start": start, "end": end}
+
+        representation = rule.get("repr")
+
+        # Parse representation if '{}' exists in it
+        if representation is not None:
+            if "{" in representation and "}" in representation:
                 group = match.group  # noqa: F841
                 custom_function = self.custom_functions  # noqa: F841
-                representation_out = eval('f"' + representation + '"')
+                if offset != 0:
+                    representation = re.sub("(group\\([0-9]+)(\\))", repl=f"\\1+{offset}\\2", string=representation)
+                representation = eval('f"' + representation + '"')
 
-            start, end = match.span(target_regex_group)
-            item = {"raw": raw, "start": start, "end": end}
+            item["repr"] = representation
 
-            # Additional metadata
-            if representation_out is not None:
-                item["repr"] = representation_out
-            if rule_id is not None:
-                item["rule_id"] = rule_id
-            if categories is not None:
-                item["categories"] = categories
+        rule_id = rule.get("id")
+        if rule_id is not None:
+            item["rule_id"] = rule_id
 
-            if return_match_obj:
+        categories = rule.get("categories")
+        if categories is not None:
+            item["categories"] = categories
+
+        if return_match_obj:
+            if self.mode is SEARCH_MODE.UNIFIED:
+                print("WARN: return_match_obj=True is NOT supported in SEARCH_MODE.UNIFIED (ignored)")
+            else:
                 item["match"] = match
 
-            result.append(item)
-
-        return result
+        return item

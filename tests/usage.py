7d3216e3fdd4b5b55f7a9ea061e18d10838dbbed
du-du-zi <jaehee_sg@naver.com>
2022-04-22 13:29:42 +0900

    test 룰 작성

--- tests/usage.py
+++ tests/usage.py
@@ -5,8 +5,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 import rexearch  # noqa : E402
 
 rx = rexearch.Rexearch()
-rx.load_json_file("sample.rules.json")
-sample_input = open("sample.input.txt", mode="rt").read()
+rx.load_json_file("tests/sample.rules.json")
+sample_input = open("tests/sample.input.txt", mode="rt").read()
 results = rx.search(sample_input)
+
 for result in results:
     print(result)

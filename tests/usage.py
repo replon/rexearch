00a7af1c18399075870ba622779c7bddc64a70c5
du-du-zi <jaehee_sg@naver.com>
2022-04-22 17:42:25 +0900

    add custom_function

--- tests/usage.py
+++ tests/usage.py
@@ -4,10 +4,18 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))  # This is just for test env
 import rexearch  # noqa : E402
 
+def Find_FE():
+    return str()
+
 rx = rexearch.Rexearch()
-rx.load_json_file("tests/sample.rules.json")
-sample_input = open("tests/sample.input.txt", mode="rt").read()
+rx.load_json_file("tests/jay.test.rules.json")
+sample_input = open("tests/jay.test.input.txt", mode="rt").read()
+rx.custom_functions["Find_FE"] = Find_FE
+
+
 results = rx.search(sample_input)
+
+
 
 for result in results:
     print(result)

ebeca5cbe6b99c930dd0ccb036d19cb273b413e6
du-du-zi <jaehee_sg@naver.com>
2022-04-26 01:12:05 +0900

    금액 관련 함수 수정

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,12 @@
-import os
-from re import I
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))  # This is just for test env
-import rexearch  # noqa : E402
-
-rx = rexearch.Rexearch()
-rx.load_json_file("tests/jay.test.rules.json")
-sample_input = open("tests/jay.test.input.txt", mode="rt").read()
-
 hangle_numbers = {'영':[0,0],'공':[0,0],'일':[1,0],'이':[2,0],'삼':[3,0],'사':[4,0],'오':[5,0],
-'육':[6,0],'칠':[7,0],'팔':[8,0],'구':[9,0],'0':[0,0],'1':[1,0],'2':[2,0],'3':[3,0],'4':[4,0],
-'5':[5,0],'6':[6,0],'7':[7,0],'8':[8,0],'9':[9,0],'십':[10,1],'백':[10**2,1],'천':[10**3,1],
-'만':[10**4,1],'억':[10**8,1],'조':[10**12,1]}
+'육':[6,0],'칠':[7,0],'팔':[8,0],'구':[9,0],
+'0':[0,0],'1':[1,0],'2':[2,0],'3':[3,0],'4':[4,0],'5':[5,0],
+'6':[6,0],'7':[7,0],'8':[8,0],'9':[9,0],
+'십':[10,1],'백':[10**2,1],
+'천':[10**3,1],'만':[10**4,1],'억':[10**8,1],'조':[10**12,1]}
 
 # custom function
-def Find_FE(price):
+def find_FE(price):
     number_table = [hangle_numbers[token] for token in price if token.isalnum()]
     
     def is_unit(table_index):
@@ -37,13 +28,20 @@ def Find_FE(price):
                 result += "+"
         result += str(number_table[i][0])     
     return format(eval(result),',') 
-
-def Find_tag(currency):
-    return currency
-
-rx.custom_functions["Find_FE"] = Find_FE
-rx.custom_functions["Find_tag"] = Find_tag
-results = rx.search(sample_input)
-
-for result in results:
-    print(result)
+    
+    
+print(find_FE('8십억 육천만'))
+print(find_FE('십억 6000만'))
+print(find_FE('10억 육천만'))
+print(find_FE('234억 육천만'))
+print(find_FE('천억 50만'))
+print(find_FE('십8만'))
+print(find_FE('십팔만'))
+print(find_FE('5억천3백'))
+print(find_FE('팔천오백만'))
+print(find_FE('일공공공칠일'))
+print(find_FE('백오'))
+print(find_FE('구'))
+print(find_FE('삼공공만'))
+print(find_FE('구천팔백사십칠만육천삼백이십오'))
+print(find_FE('5백1만'))

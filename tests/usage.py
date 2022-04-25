b8ae70e645e2c07568d8bc31109f3f403b2fafa8
du-du-zi <jaehee_sg@naver.com>
2022-04-25 18:06:14 +0900

    금액 추출 정규식 수정

--- tests/usage.py
+++ tests/usage.py
@@ -10,11 +10,23 @@ def Find_FE():
 rx = rexearch.Rexearch()
 rx.load_json_file("tests/jay.test.rules.json")
 sample_input = open("tests/jay.test.input.txt", mode="rt").read()
-rx.custom_functions["Find_FE"] = Find_FE
-
 
 results = rx.search(sample_input)
 
+
+hangle_numbers = {'일':1,'이':2,'삼':3,'사':4,'오':5,'육':6,'칠':7,'팔':8,'구':9}
+
+#custom function
+def find_FE(price):
+  
+    #일단위(일~구) 숫자 변환
+    tmp = str([hangle_numbers.get(token, token) for token in price])
+
+    
+
+    #숫자와 단위 구분
+
+    #단위 합산
 
 
 for result in results:
